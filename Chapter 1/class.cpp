0082d11c98faf1ae85b9da880dd11b1621393306
vivekup3424 <vivek.hygfrt3424@gmail.com>
2021-12-23 21:53:08 +0530

    studying the notrs of 6.096

---
+++ Chapter 1/class.cpp
@@ -0,0 +1,13 @@
+//Sale_item class represent the total revenue, number of copies sold, and average sales price for a book
+#include <iostream>
+//#include <Sales_item.h>
+
+int main(){
+  //  Sales_item book;
+    //read ISBN, number of copies sold, and sales price
+    //std::cin >> book;
+    //write ISBN, number of copies sold and average price
+    //std::cout << book <<std::endl;
+    return 0;
+}
+
---
+++ Chapter 2/practice.py
@@ -0,0 +1,5 @@
+sum = 0
+for i in range(0,17):
+    sum += 2**i
+print(sum)
+print(2**16)
